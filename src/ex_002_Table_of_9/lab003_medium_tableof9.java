4fddfe51233cfbf3bc7766f0cbd87624dc5bcabc
Yogesh Patel <yogesh@beaconrisk.co>
2025-09-21 19:25:02 +0530

    complex table

---
+++ src/ex_002_Table_of_9/lab003_medium_tableof9.java
@@ -0,0 +1,26 @@
+package ex_002_Table_of_9;
+
+public class lab003_medium_tableof9 {
+
+
+
+            public static void main(String[] args) {
+
+            // Print the table of 9 or 5 using the System.out.printf
+            int num = 9;
+            // 9x1=9
+            // 9x2=18
+            // int -> %d, String -> %s, Char -%c, float -> %f
+            System.out.printf("%dx1=%d",num,num*1);
+            System.out.println();
+            System.out.printf("%dx2=%d",num,num*2).println();
+            System.out.printf("%dx3=%d",num,num*3).println();
+            System.out.println("...");
+            System.out.printf("%dx9=%d",num,num*9).println();
+            System.out.printf("%dx10=%d",num,num*10).println();
+
+
+
+        }
+
+}
