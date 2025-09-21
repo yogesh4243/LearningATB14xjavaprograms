662a558cd3400de92a54f54212fd3e700914aa05
Yogesh Patel <yogesh@beaconrisk.co>
2025-09-21 19:08:47 +0530

    commited

---
+++ src/ex_002_Table_of_9/lab002_simple_table_of9.java
@@ -0,0 +1,18 @@
+package ex_002_Table_of_9;
+
+public class lab002_simple_table_of9 {
+    public static void main(String[] args) {
+        int num = 9;
+        System.out.printf ("9*1=%d%n", num );
+        System.out.printf("9*2=%d%n", num*2);
+        System.out.printf("9*3=%d%n", num*3);
+        System.out.printf("9*4=%d%n", num*4);
+
+        System.out.printf("9*5=%d%n", num*5);
+        System.out.printf("9*6=%d%n", num*6);
+        System.out.printf("9*7=%d%n", num*7);
+        System.out.printf("9*8=%d%n", num*8);
+        System.out.printf("9*9=%d%n", num*9);
+        System.out.printf("9*10=%d%n", num*10);
+    }
+}
