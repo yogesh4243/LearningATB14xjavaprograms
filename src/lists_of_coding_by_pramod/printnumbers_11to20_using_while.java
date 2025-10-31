9f247fca1c3143caa299e80377c273a84f96934b
Yogesh Patel <yogesh@beaconrisk.co>
2025-10-31 17:38:54 +0530

    10 to 20

---
+++ src/lists_of_coding_by_pramod/printnumbers_11to20_using_while.java
@@ -0,0 +1,9 @@
+package lists_of_coding_by_pramod;
+
+public class printnumbers_11to20_using_while {
+    public static void main(String[] args) {
+        int i = 11;
+        while (i<=20)
+        {System.out.println(i);i++;}
+    }
+}
