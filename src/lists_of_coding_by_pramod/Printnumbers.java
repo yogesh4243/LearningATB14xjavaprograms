ce89f6291d400afeb6c7fa9a0162616d6db1e604
Yogesh Patel <yogesh@beaconrisk.co>
2025-10-31 12:03:10 +0530

    20

---
+++ src/lists_of_coding_by_pramod/Printnumbers.java
@@ -0,0 +1,9 @@
+package lists_of_coding_by_pramod;
+
+public class Printnumbers {
+    public static void main(String[] args) {
+        for (int i = 1; i <=10 ; i++) {
+            System.out.println(i);
+        }
+    }
+}
