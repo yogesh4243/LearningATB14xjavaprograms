95d4393ce8c741945a9cff37dfdd4173ce0838e8
Yogesh Patel <yogesh@beaconrisk.co>
2025-10-31 18:39:21 +0530

    for loop even

---
+++ src/loop_for_eg/string_iteration.java
@@ -0,0 +1,10 @@
+package loop_for_eg;
+
+public class string_iteration {
+    public static void main(String[] args) {
+        for (int i = 0; i <=5; i++) {
+            System.out.println("hello");
+
+        }
+    }
+}
