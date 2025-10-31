09d5d1d053619d8e65bf9059d44670823e81a314
Yogesh Patel <yogesh@beaconrisk.co>
2025-10-31 17:03:51 +0530

    10 to 20

--- src/lists_of_coding_by_pramod/printnumbers_10to20.java
+++ src/lists_of_coding_by_pramod/printnumbers_10to20.java
@@ -1,9 +1,10 @@
 package lists_of_coding_by_pramod;
 
-public class Printnumbers {
+public class printnumbers_10to20 {
     public static void main(String[] args) {
-        for (int i = 1; i <=20 ; i++) {
+        for (int i = 10; i <=20 ; i++) {
             System.out.println(i);
+
         }
     }
 }
